58c1cdb00fb4eee58d68d59571c14de1afa80972
Nikita233 <55126662+Nikita233@users.noreply.github.com>
2019-11-22 09:24:16 +0200

    1

--- myproject/src/app/user.module.ts
+++ myproject/src/app/user.module.ts
@@ -1,7 +1,14 @@
-export class User{
+declare interface User {
     id: number;
-    
+  
     __unicode__: string;
     name: string;
     short_name: string;
-}
+  }
+  
+  declare interface PaginatedResponseInterface<T> {
+    count: number;
+    next: string;
+    previous: string;
+    results: T[];
+  }
